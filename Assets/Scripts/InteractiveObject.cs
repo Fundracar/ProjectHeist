ca57809c650452c71517db74c6eb7a3bb4130c66
Flatheet <77152086+Flatheet@users.noreply.github.com>
2021-01-26 15:39:21 +0100

    Revert "Revert "Gestion du personnage""
    This reverts commit 703c5a5017cf759b763290513b6d18bae91da798.

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    protected bool isActive = false;
+    
+    public void OnInteraction()
+    {
+        ActiveObject();
+        PlayAnim();
+    }
+
+    // Play the object Animation
+    private void PlayAnim()
+    {
+        Debug.Log("Active Animation");
+    }
+
+    // Active the object effect
+    protected virtual void ActiveObject()
+    {
+        
+    }
+}
