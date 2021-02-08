caad6f32d20d2f5437d11b84bdabc171b0ebfe19
baobob <bonaventure.bob@gmail.com>
2021-02-08 13:36:42 +0100

    Scripts and prefab
    Added
    Contract Script and tempGameManager Script
    New tags

---
+++ Assets/TempGameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGameManager : MonoBehaviour
+{
+    public Dictionary<int, Contract> dictOfContracts;
+    public List<GameObject> listOfContractsPrefab;
+    public Contract storedContract;
+
+
+    public Contract GetContractInDict(int linkedContractId)
+    {
+        Contract foundContract = dictOfContracts[linkedContractId];
+        return foundContract;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dictionary<int, Contract> dictOfContracts =
+        new Dictionary<int, Contract>();
+
+        foreach (GameObject currentContractObject in listOfContractsPrefab)
+        {
+            Contract currentContractComponent = currentContractObject.GetComponent<Contract>();
+            dictOfContracts.Add(currentContractComponent.contractId, currentContractComponent);
+            Debug.Log("+1ContractInDict");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
