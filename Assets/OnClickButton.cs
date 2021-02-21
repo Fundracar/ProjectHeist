dbf41980328d4727c3209d2f68214a228a6f6924
baobob <bonaventure.bob@gmail.com>
2021-02-21 16:08:25 +0100

    Tools&Crew workflow
    Added
    - scripts : Tools, Crew, OnClickButton, modification of TempGameManager
    - Player can now see all his unlocked tools and crew in the planning phase and select one of each
    -Imported some tests assets
    - New tags necessary in the scripts
    - New prefabs
    Next : Get the selectedTool and selectedCrew info

---
+++ Assets/OnClickButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickButton : MonoBehaviour
+{
+    public Image buttonImage;
+    public Tools selectedTool;
+    public Crew selectedCrew;
+
+
+    //Method to display the clicked tool in the selected tool slot
+    public void DisplaySelectedTool()
+    {
+        Image selectedToolImage = GameObject.FindGameObjectWithTag("SelectedToolDisplayer").GetComponent<Image>();
+        selectedToolImage.sprite = buttonImage.sprite;
+        selectedToolImage.color = buttonImage.color;
+        //Debug.Log("test");
+        Tools toolTest = GameObject.FindGameObjectWithTag("TempGameManager").GetComponent<Tools>();
+        Debug.Log("test1");
+    }
+
+    //Method to display the clicked crew in the selected crew slot
+    public void DisplaySelectedCrew()
+    {
+        Image selectedCrewMemberImage = GameObject.FindGameObjectWithTag("SelectedCrewMemberDisplayer").GetComponent<Image>();
+        selectedCrewMemberImage.sprite = buttonImage.sprite;
+        selectedCrewMemberImage.color = buttonImage.color;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/TempGameManager.cs
+++ Assets/TempGameManager.cs
@@ -13,6 +13,79 @@ public class TempGameManager : MonoBehaviour
     public Vector3 infoBoxOffSet;
     public int currentPlayerRep;
     public int playerRep;
+    public List<GameObject> listOfToolsPrefab;
+    public Dictionary<int, Tools> dictOfAllTools;
+    public Dictionary<int, Tools> dictOfUnlockedTools;
+    public List<GameObject> listOfToolButtonPrefab;
+    public List<GameObject> listOfCrewMemberPrefab;
+    public Dictionary<int, Crew> dictOfAllCrewMembers;
+    public Dictionary<int, Crew> dictOfUnlockedCrewMembers;
+    public List<GameObject> listOfCrewMemberButton;
+    public Tools currentSelectedTool;
+    public Crew currentSelectedCrew;
+    
+    //public Tools selectedTool
+    //public Crew selectedCrew
+    //When OnClickButton Method called, update selected
+
+    //Method to Display all unlocked Crew Members
+    public void DisplayUnlockedCrewMembers()
+    {
+        int i = 0;
+        foreach (KeyValuePair<int, Crew> unlockedCrewMemberIterator in dictOfUnlockedCrewMembers)
+        {
+            listOfCrewMemberButton[i].SetActive(true);
+            //Debug.Log(listOfCrewMemberButton[i].activeSelf);
+            Image img = listOfCrewMemberButton[i].GetComponent<Image>();
+            img.sprite = unlockedCrewMemberIterator.Value.crewSprite.sprite;
+            img.color = unlockedCrewMemberIterator.Value.crewSprite.color;
+            i += 1;
+        }
+    }
+
+    //Method to get all current unlocked CrewMembers
+    public void ResolveUnlockedCrewMembers()
+    {
+        foreach (KeyValuePair<int, Crew> crewMemberIterator in dictOfAllCrewMembers)
+        {
+            if (crewMemberIterator.Value.crewReputationTreshold <= playerRep)
+            {
+                dictOfUnlockedCrewMembers.Add(crewMemberIterator.Key, crewMemberIterator.Value);
+                Debug.Log("+1 crewMember unlocked");
+            }
+        }
+    }
+
+    //Method to Display all unlocked Tools
+    public void DisplayUnlockedTools()
+    {
+        int i = 0;
+        foreach (KeyValuePair<int, Tools> unlockedToolIterator in dictOfUnlockedTools)
+        {
+            listOfToolButtonPrefab[i].SetActive(true);
+            Debug.Log(listOfToolButtonPrefab[i].activeSelf);
+            Image img = listOfToolButtonPrefab[i].GetComponent<Image>();
+            img.sprite = unlockedToolIterator.Value.toolSprite.sprite;
+            img.color = unlockedToolIterator.Value.toolSprite.color;
+            i += 1;
+        }
+    }
+
+
+    //Method to get all current unlocked Tools
+    public void ResolveUnlockedTools()
+    {
+        foreach (KeyValuePair<int, Tools> toolIterator in dictOfAllTools)
+        { 
+            if (toolIterator.Value.toolReputationTreshold <= playerRep)
+            {
+                dictOfUnlockedTools.Add(toolIterator.Key, toolIterator.Value);
+                Debug.Log("+1 tool unlocked");
+            }
+                           
+        }
+        
+    }
 
 
     //Method to destroy the info box clone after hovering
@@ -53,7 +126,7 @@ public class TempGameManager : MonoBehaviour
 
     }
 
-
+    //Method to display all the contract informations on the side and bottom description bar in the selectionContract Panel
     public void ContractAllInfoDisplay(int linkedContractId)
     {
         Contract foundContract = dictOfContracts[linkedContractId];
@@ -83,19 +156,65 @@ public class TempGameManager : MonoBehaviour
 
     }
 
+    //Method to display the needed contract informations in the planning phase
+    public void PlanningPhaseContractInfoDisplay()
+    {
+        
+        Contract selectedContract = storedContract;
+
+        Text planningDisplayContractName = GameObject.FindGameObjectWithTag("PlanningPhase.contractName").GetComponent<Text>();
+        planningDisplayContractName.text = selectedContract.contractName;
+        Text planningDisplayContractVigilanceScore = GameObject.FindGameObjectWithTag("PlanningPhase.contractVigilanceScoreValue").GetComponent<Text>();
+        planningDisplayContractVigilanceScore.text = selectedContract.vigilanceScore.ToString();
+        Text planningDisplayContractMainObjective = GameObject.FindGameObjectWithTag("PlanningPhase.contractMainObjective").GetComponent<Text>();
+        planningDisplayContractMainObjective.text = selectedContract.contractMainObjective;
+        Text planningDisplayContractConstraints = GameObject.FindGameObjectWithTag("PlanningPhase.contractConstraints").GetComponent<Text>();
+        planningDisplayContractConstraints.text = selectedContract.contractConstraints;
+        Text planningDisplayContractBonusObjective = GameObject.FindGameObjectWithTag("PlanningPhase.contractBonusObjective").GetComponent<Text>();
+        planningDisplayContractBonusObjective.text = selectedContract.contractBonusObjective;
+
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         dictOfContracts = new Dictionary<int, Contract>();
+        dictOfAllTools = new Dictionary<int, Tools>();
+        dictOfUnlockedTools = new Dictionary<int, Tools>();
+        dictOfAllCrewMembers = new Dictionary<int, Crew>();
+        dictOfUnlockedCrewMembers = new Dictionary<int, Crew>();
 
         foreach (GameObject currentContractObject in listOfContractsPrefab)
         {
             Contract currentContractComponent = currentContractObject.GetComponent<Contract>();
             dictOfContracts.Add(currentContractComponent.contractId, currentContractComponent);
             //Debug.Log("+1ContractInDict");
-
         }
 
+        foreach (GameObject availableTool in listOfToolsPrefab)
+        {
+            Tools t = availableTool.GetComponent<Tools>();
+            dictOfAllTools.Add(t.toolId, t);
+            Debug.Log("+1 tool");
+        }
+
+        foreach (GameObject availableCrewMember in listOfCrewMemberPrefab)
+        {
+            Crew c = availableCrewMember.GetComponent<Crew>();
+            dictOfAllCrewMembers.Add(c.crewId, c);
+            Debug.Log("+1 Crew Member");
+        }
+
+        //Add save informations before RaisePlayerRep and ResolveUnlockedTools
+        //LoadSaveInfo();
+        RaisePlayerRep();
+        ResolveUnlockedTools();
+        DisplayUnlockedTools();
+        ResolveUnlockedCrewMembers();
+        DisplayUnlockedCrewMembers();
     }
 
     // Update is called once per frame
