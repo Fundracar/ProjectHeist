ea20f459c2a23e6379f508678cf9ccd35512e286
Ryan Amarat <ryan.amarat@gmail.com>
2021-02-23 17:39:41 +0100

    Added Music Manager
    Music Manager that plays the infiltration song during the level 1

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+       public AudioSource gameMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMusic.Play(0);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
