7ea004c5ef9180e6a99b17992304bbb218ccb415
baobob <bonaventure.bob@gmail.com>
2021-03-13 17:06:45 +0100

    ParticleSystem and minor menu modifications

---
+++ Assets/BehaviourParticleSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourParticleSystem : MonoBehaviour
+{
+    public bool particleState;
+    //public GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivateParticleSystem();
+    }
+
+
+    public void DestroyParticleSystem()
+    {
+        particleState = GameObject.FindGameObjectWithTag("ParticleSystem").GetComponent<ParticleSystem>().enableEmission = false;
+        //test = GameObject.FindGameObjectWithTag("ParticleSystem").SetActive(false);
+    }
+
+
+    public void ActivateParticleSystem()
+    {
+        particleState = GameObject.FindGameObjectWithTag("ParticleSystem").GetComponent<ParticleSystem>().enableEmission = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
